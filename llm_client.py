cb487b578dbc41e34ce8ddcc93d633451d95875b
Nikesh Maurya <nikemaurya1996@gmail.com>
2025-09-22 09:14:22 +0000

    llm client reference

--- llm_client.py
+++ llm_client.py
@@ -1,5 +1,6 @@
 # llm_client.py
 from langchain_perplexity import ChatPerplexity
-from config import PERPLEXITY_MODEL, PERPLEXITY_API_KEY
-
-chat = ChatPerplexity(model=PERPLEXITY_MODEL, api_key=PERPLEXITY_API_KEY)
+from config import PERPLEXITY_MODEL
+import streamlit as st
+my_key = st.secrets["PERPLEXITY_API_KEY"]
+chat = ChatPerplexity(model=PERPLEXITY_MODEL, api_key=my_key)
