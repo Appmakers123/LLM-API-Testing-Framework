65137ca06132d12d675ac28073ea924255d36d4b
Nikesh Maurya <nikemaurya1996@gmail.com>
2025-09-22 09:24:25 +0000

    path

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-KNOWLEDGE_FOLDER = r"./workspaces/LLM-API-Testing-Framework/Document"
-OPENAPI_YAML_PATH = r"./workspaces/LLM-API-Testing-Framework/testdata/sampleTest.yaml"
-CSV_OUTPUT = r"./workspaces/LLM-API-Testing-Framework/reports/api_exec_report_consent.csv"
+KNOWLEDGE_FOLDER = r"./Document"
+OPENAPI_YAML_PATH = r"./testdata/sampleTest.yaml"
+CSV_OUTPUT = r"./reports/api_exec_report_consent.csv"
 BASE_URL_OVERRIDE = "https://api.restful-api.dev"
 VERIFY_SSL = False
 REQUEST_TIMEOUT_SECS = 30
